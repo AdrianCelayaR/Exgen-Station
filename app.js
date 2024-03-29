f1569e74f0ca66927f785afa5c1c0ed8d4a3ccdd
VegaAxel318 <AxelEVega318@gmail.com>
2024-03-28 22:11:13 -0700

    Primera implementación de AR
    Añadida la ruta /arcam para probar la AR. Uso de cámara y activación por un marcador.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var arRouter = require('./routes/ar');
 
 var app = express();
 
@@ -17,5 +18,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/arcam',arRouter);
 
 module.exports = app;
