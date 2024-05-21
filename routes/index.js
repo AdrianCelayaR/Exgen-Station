57755ba48c0d0a3ebcbb05147f12da352be16ff4
Oscar Plata <lopez.oscar24@uabc.edu.mx>
2024-05-21 02:28:01 -0700

    Update Menu AR
    - Se agrego botn de menu
    - Se manda usuario actual
    - menu flotante
    - arreglos en ui de AR

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,31 @@
-var express = require('express');
+var express = require("express");
 const models = require("../models");
-const dotenv = require('dotenv');
-const jsonwebtoken = require('jsonwebtoken');
-const path = require('path');
+const dotenv = require("dotenv");
+const jsonwebtoken = require("jsonwebtoken");
+const path = require("path");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', async function(req, res, next) {
+router.get("/", async function (req, res, next) {
   const cookieJWT = req.cookies["jwt"];
   if (!cookieJWT) {
-    return res.render(path.join(__dirname, '../public/views/index'), { title: 'Exgen Station', current_user: null });
+    return res.render(path.join(__dirname, "../public/views/index"), {
+      title: "Exgen Station",
+      current_user: null,
+    });
   }
-  
+
   const decoded = jsonwebtoken.verify(cookieJWT, process.env.JWT_SECRET);
   const user_rol = await models.userrols.findOne({
     where: {
-      userId: decoded.id
-    }
+      userId: decoded.id,
+    },
   });
   const current_user = await models.users.findOne({
-      include: models.userrols,
-      where: {
-          id: decoded.id
-      }
+    include: models.userrols,
+    where: {
+      id: decoded.id,
+    },
   });
   let rol = null;
   // if (user_rol) {
@@ -33,78 +36,55 @@ router.get('/', async function(req, res, next) {
   //   });
   // }
   if (user_rol.roleId === 1) {
-    rol = 'admin';
+    rol = "admin";
   }
-  
-  res.render(path.join(__dirname, '../public/views/index'), { title: 'Exgen Station' , current_user: current_user, rol: rol});
+
+  res.render(path.join(__dirname, "../public/views/index"), {
+    title: "Exgen Station",
+    current_user: current_user,
+    rol: rol,
+  });
 });
 
-router.get('/about', function(req, res, next) {
-  res.render(path.join(__dirname, '../public/views/about'), { title: 'Exgen Station' });
+router.get("/about", function (req, res, next) {
+  res.render(path.join(__dirname, "../public/views/about"), { title: "Exgen Station" });
 });
 
-router.get('/contact', function(req, res, next) {
-  res.render(path.join(__dirname, '../public/views/contact'), { title: 'Exgen Station' });
+router.get("/contact", function (req, res, next) {
+  res.render(path.join(__dirname, "../public/views/contact"), { title: "Exgen Station" });
 });
 
-router.get('/arcam', async function(req, res, next) {
+router.get("/arcam", async function (req, res, next) {
   const markersAndModels = await models.markers.findAll({
     where: {
-      activo: true
-    }
+      activo: true,
+    },
   });
 
-  // const markersAndModels = [
-  //   {
-  //     marker: {
-  //       preset: "Poke",
-  //       patt: "assets/patterns/pattern-pokebal.patt",
-  //       position: "0 0 0",
-  //       rotation: "0 0 0",
-  //       scale: "1 1 1",
-  //     },
-  //     model: {
-  //       src: "assets/models/pokeballDUO.glb",
-  //       position: "0 0 0",
-  //       rotation: "0 270 90",
-  //       scale: ".25 .25 .25",
-  //       animConf: "loop:repeat",
-  //     },
-  //     text:{
-  //       data:"Has Encontrado 2 Pokeballs!!",
-  //       color:"pink",
-  //       position:"-1 0 1",
-  //       rotation:"250 0 0",
-  //       scale:"0.75 0.75 0.75"
-  //     }
-  //   },
-  //   {
-  //     marker: {
-  //       preset: "Bird",
-  //       patt: 'assets/patterns/pattern-Pajaro.patt',
-  //       position: "0 0 0",
-  //       scale: "1 1 1",
-  //       rotation: "0 0 0",
-  //     },
-  //     model: {
-  //       src: 'assets/models/birds.glb',
-  //       position: "0 0 0",
-  //       scale: ".25 .25 .25",
-  //       rotation: "0 0 0",
-  //       animConf: "loop:repeat",
-  //     },
-  //     text:{
-  //       data:"Hola Pollitos",
-  //       color:"green",
-  //       position:"-.5 0 1",
-  //       rotation:"250 0 0",
-  //       scale:"0.75 0.75 0.75"
-  //     }
-  //   },
-  // ];
-  res.render(path.join(__dirname, '../public/views/ar'), { 
-    title: 'Exgen Station',
-    markersAndModels: markersAndModels
+  const cookieJWT = req.cookies["jwt"];
+
+  if (!cookieJWT) {
+    return res.render(path.join(__dirname, "../public/views/ar"), {
+      title: "Exgen Station",
+      markersAndModels: markersAndModels,
+      currentUser: null,
+    });
+  }
+
+  const decoded = jsonwebtoken.verify(cookieJWT, process.env.JWT_SECRET);
+  const current_user = await models.users.findOne({
+    include: models.userrols,
+    where: {
+      id: decoded.id,
+    },
+  });
+  // Convertir currentUser a JSON y pasarlo a la plantilla
+  const currentUserJSON = JSON.stringify(current_user);
+  console.log(currentUserJSON);
+  res.render(path.join(__dirname, "../public/views/ar"), {
+    title: "Exgen Station",
+    markersAndModels: markersAndModels,
+    currentUser: current_user,
   });
 });
 module.exports = router;
